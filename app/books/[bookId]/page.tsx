f4872edb2df8f976c4f5ae730debb40593d18989
andylitvinov-design <andy.litvinov@gmail.com>
2026-09-24 01:35:37 -0400

    Fix translated reader import newline

--- app/books/[bookId]/page.tsx
+++ app/books/[bookId]/page.tsx
@@ -3,7 +3,8 @@ import type { Metadata } from "next";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-import { Book02Reference } from "@/components/book-02-reference";\nimport { TranslatedReaderContent } from "@/components/translated-reader-content";
+import { Book02Reference } from "@/components/book-02-reference";
+import { TranslatedReaderContent } from "@/components/translated-reader-content";
 import { books, getBookById } from "@/data/library";
 import { localizedBookText } from "@/data/library-localization";
 import { loadReaderDocument } from "@/data/reader-content";
